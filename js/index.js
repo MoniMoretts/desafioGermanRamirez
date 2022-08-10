e57a62db8bcad2641ae81bfd621b509027fdc8ad
German Ramirez <gerramirez2018@gmail.com>
2022-08-09 22:40:40 -0300

    entrega final 2

--- js/index.js
+++ js/index.js
@@ -53,19 +53,62 @@ function mostrarProductos(categoria) {
             break;
     }
 }
-
+const indumentaria1 = {
+    prenda: "Jean",
+    valor: "9000ars"
+}
+const indumentaria2 = {
+    prenda: "Buzo",
+    valor: "15000ars"
+}
+const indumentaria3 = {
+    prenda: "Remera",
+    valor: "5000ars"
+}
+const indumentaria = [indumentaria1.prenda, indumentaria1.valor, indumentaria2.prenda, indumentaria2.valor, indumentaria3.prenda, indumentaria3.valor]
+    
 function mostrarIndumentaria() {
-    const indumentaria = ["Jean:100usd", "Buzo:150usd", "Remera:50usd"]
+    
     mostrarProductosCategoria(indumentaria)
 }
 
+const calzado1 = {
+    prenda: "Jordan 3",
+    valor: ("40000ars")
+}
+const calzado2 = {
+    prenda: "Botas Dr Martens",
+    valor: "50000ars"
+}
+const calzado3 = {
+    prenda: "Vans Classic",
+    valor: "13000ars"
+}
+
+const calzado = [calzado1.prenda, calzado1.valor, calzado2.prenda, calzado2.valor, calzado3.prenda, calzado3.valor]
+
 function mostrarCalzado() {
-    const calzado = ["Zapatilla:250usd", "Zapato:200usd", "Botas:400usd"]
+    
     mostrarProductosCategoria(calzado)
 }
 
+const accesorio1 = {
+    prenda: "Collar Plata",
+    valor: "1500ars"
+}
+const accesorio2 = {
+    prenda: "Collar Oro",
+    valor: "10000ars"
+}
+const accesorio3 = {
+    prenda: "Pulsera Plata",
+    valor: "500ars"
+}
+
+const accesorios = [accesorio1.prenda, accesorio1.valor, accesorio2.prenda, accesorio2.valor, accesorio3.prenda, accesorio3.valor]
+
 function mostrarAccesorios() {
-    const accesorios = ["Collar:20usd", "Pulsera:10usd"]
+    
     mostrarProductosCategoria(accesorios)
 }
 
@@ -75,7 +118,7 @@ function mostrarProductosCategoria(productos) {
     productos.forEach((producto) => {
         const nodoDiv = document.createElement("div")
         nodoDiv.classList.add("lista");
-        nodoDiv.innerText = producto;
+        nodoDiv.innerText =JSON.stringify (producto);
         nodoContenedor.appendChild(nodoDiv)
     })
 }
@@ -92,49 +135,48 @@ function BotonComprar() {
 
 }
 
-function comprar()
-{
-    let numCompras = prompt("Ingrese el numero de productos que desea comprar")
+function comprar() {
+    let numCompras = prompt("Ingrese la cantidad de productos que desea comprar")
     let contador = 0;
     let total = 0;
-    if (NaN)
-        {
-            alert("ERROR, EL DATO INGRESADO ES INVALIDO")
-            comprar()
-        }
-    else{
-        while(contador<numCompras)
-            {
-                let cifras = Number(prompt(`Ingrese el monto del producto ${contador+1}`))
-                if (cifras>0){
-                    total+=cifras
-                }
-                contador++;
+    if (NaN) {
+        alert("ERROR, EL DATO INGRESADO ES INVALIDO")
+        comprar()
+    }
+    else {
+        while (contador < numCompras) {
+            let cifras = Number(prompt(`Ingrese el monto del producto ${contador + 1} ${JSON.stringify(indumentaria +" "+ calzado+" "+ accesorios)}`))
+            if (cifras > 0) {
+                total += cifras
             }
-            alert(`Su monto total (mas iva incluido) es $${total+(total*21/100)}`)
-            let cuotas = Number(prompt("Ingrese el numero de cuotas en las que desee abonar (1,3,6,9 o 12)"))
-                switch(cuotas)
-                {
-                    case 1:
-                        alert(`su monto final es $${total+(total*21/100)}`);
-                        break;
-                    case 3:
-                        alert(`su total (mas iva incluido) son 3 cuotas de $${(total+(total*0.21))/3}`);
-                        break;
-                    case 6:
-                        alert(`su total (mas iva incluido) son 6 cuotas de $${(total+(total*0.21))/6}`);
-                        break;
-                    case 9:
-                        alert(`su total (mas iva incluido) son 9 cuotas de $${(total+(total*0.21))/9}`);
-                        break;
-                    case 12:
-                        alert(`su total (mas iva incluido) son 12 cuotas de $${(total+(total*0.21))/12}`);
-                        break;
-                    default:
-                        alert("ERROR, EL DATO INGRESADO ES INVALIDO")
-                        login()
-                        break;
-                }      
+            else {
+                alert ("Coloque un monto correcto")
+                comprar()
+            }
+            contador++;
+        }
+        alert(`Su monto total (mas iva incluido) es $${total + (total * 21 / 100)}`)
+        let cuotas = Number(prompt("Ingrese el numero de cuotas en las que desee abonar (1,3,6,9 o 12)"))
+        switch (cuotas) {
+            case 1:
+                alert(`su monto final es $${total + (total * 21 / 100)}`);
+                break;
+            case 3:
+                alert(`su total (mas iva incluido) son 3 cuotas de $${(total + (total * 0.21)) / 3}`);
+                break;
+            case 6:
+                alert(`su total (mas iva incluido) son 6 cuotas de $${(total + (total * 0.21)) / 6}`);
+                break;
+            case 9:
+                alert(`su total (mas iva incluido) son 9 cuotas de $${(total + (total * 0.21)) / 9}`);
+                break;
+            case 12:
+                alert(`su total (mas iva incluido) son 12 cuotas de $${(total + (total * 0.21)) / 12}`);
+                break;
+            default:
+                alert("ERROR, EL DATO INGRESADO ES INVALIDO")
+                break;
+        }
     }
 }
 
